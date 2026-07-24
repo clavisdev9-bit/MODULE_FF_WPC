2296a8ad382082c61cf6544d474bef43cd4cf65a
zephyrx0 <ganelajeisa@gmail.com>
2026-07-24 10:47:16 +0700

    mixin relocation

--- addons/freight_forwarding/models/sea/cargo_info_mixin.py
+++
@@ -1,2 +0,0 @@
-# Model dipindah ke models/common/cargo_info_mixin.py
-# _name = "freight.sea.cargo.info.mixin"
--- addons/freight_forwarding/models/sea/freight_items_line_mixin.py
+++
@@ -1,2 +0,0 @@
-# Model dipindah ke models/common/freight_items_line_mixin.py
-# _name = "freight.sea.items.line.mixin"
