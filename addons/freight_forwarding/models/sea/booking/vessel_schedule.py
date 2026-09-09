643906e0cda00a0d322c43100bb9b0ee14fcb2cf
fauziahRN <fzhnadin@gmail.com>
2026-09-09 14:50:45 +0700

    FF-61-report-air-notice-of-arrival

--- addons/freight_forwarding/models/sea/booking/vessel_schedule.py
+++ addons/freight_forwarding/models/sea/booking/vessel_schedule.py
@@ -12,13 +12,6 @@ class SeaBookingVesselSchedule(models.Model):
             "Vessel Schedule harus terhubung ke Booking atau Jobsheet.",
         )
     ]
-
-    # NOTE (FF-22): sebelumnya FK ke freight.sea.booking.shipment.info
-    # (model perantara yang sudah dihapus). Model ini dipakai bersama oleh
-    # Booking dan HBL lewat field vessel_schedule_id di
-    # freight.sea.shipment.info.mixin, jadi FK diarahkan langsung ke
-    # booking_id / hbl_id -- keduanya opsional (hanya salah satu yang
-    # terisi tergantung record ini dibuat dari konteks Booking atau HBL).
     booking_id = fields.Many2one(
         "freight.sea.booking",
         string="Booking",
