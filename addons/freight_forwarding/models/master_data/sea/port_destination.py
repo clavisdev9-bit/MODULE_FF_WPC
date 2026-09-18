658bc97f9abcd3c3dabfb1c15201de6db1883cb5
fauziahRN <fzhnadin@gmail.com>
2026-09-18 11:32:42 +0700

    ff-69-enhance-port-destination

--- addons/freight_forwarding/models/master_data/sea/port_destination.py
+++ addons/freight_forwarding/models/master_data/sea/port_destination.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FreightPortDestination(models.Model):
@@ -9,4 +9,16 @@ class FreightPortDestination(models.Model):
         'freight.port', required=True, ondelete='cascade',
     )
     destination_city_id = fields.Many2one('res.city', string='Destination City')
+    destination_country_id = fields.Many2one(
+        'res.country',
+        string='Country',
+        compute='_compute_destination_country_id',
+        store=True,
+        readonly=True,
+    )
     no_of_day = fields.Integer(string='No. Of Day')
+
+    @api.depends('destination_city_id')
+    def _compute_destination_country_id(self):
+        for rec in self:
+            rec.destination_country_id = rec.destination_city_id.country_id or False
